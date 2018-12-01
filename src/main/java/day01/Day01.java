a390e619e50aca9f5e4f6962e0de839a1fd2aa88
sandell <sandell@crashandlearn.se>
2018-12-01 16:46:22 +0100

    Marianne & Johan solved Day One Part One. First time Marianne solved any Advent of Code puzzles. This is cool!

--- src/main/java/day01/Day01.java
+++ src/main/java/day01/Day01.java
@@ -1,4 +1,27 @@
 package day01;
 
+import java.util.ArrayList;
+
 public class Day01 {
+
+    static int getFrequency(String input) {
+        int startFrequency = 0;
+        int currentFrequency = startFrequency;
+        ArrayList<Integer> changingFrequencies = getFrequenciesFromString(input);
+        for(int frequencyInt : changingFrequencies) {
+            currentFrequency = currentFrequency + frequencyInt;
+        }
+        return currentFrequency;
+    }
+
+    static ArrayList<Integer> getFrequenciesFromString(String input) {
+         String[] frequencyArray = input.split(", ");
+        ArrayList<Integer> frequencies =  new ArrayList<>();
+        for (String frequencyString : frequencyArray) {
+            int frequencyInt = Integer.valueOf(frequencyString);
+            frequencies.add(frequencyInt);
+        }
+        return frequencies;
+    }
+
 }
