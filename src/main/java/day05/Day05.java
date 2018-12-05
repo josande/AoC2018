5a1948c323327d6d0663f8a4d01aaa4f1d702669
sandell <sandell@crashandlearn.se>
2018-12-05 06:52:18 +0100

    Day5

--- src/main/java/day05/Day05.java
+++ src/main/java/day05/Day05.java
@@ -1,4 +1,41 @@
 package day05;
 
 public class Day05 {
+
+
+    static int getRemainingUnitsLength(String input) {
+        String currentUnits = input;
+        while (true) {
+            int length = currentUnits.length();
+            String newString = currentUnits;
+            for (int i = length-1; i > 0; i--) {
+                if ( (currentUnits.charAt(i) == currentUnits.charAt(i-1)+32) ||
+                     (currentUnits.charAt(i) == currentUnits.charAt(i-1)-32) ) {
+                    newString=newString.substring(0,i-1)+newString.substring(i+1,newString.length());
+                    i --;
+                }
+            }
+            currentUnits=newString;
+            if (newString.length() == length) {
+                return newString.length();
+            }
+
+        }
+    }
+
+
+    static int getBestReducedPolymerLength(String input) {
+    int bestSoFar=10000;
+        for (int i= 65; i<=90;i++) {
+            String toRemoveUpperCase = "" + (char) i;
+            String toRemoveLowerCase = "" + (char) (i+32);
+
+            String tempString = input.replaceAll(toRemoveUpperCase,"").replaceAll(toRemoveLowerCase,"");
+            int currentLength=getRemainingUnitsLength(tempString);
+            bestSoFar=Math.min(bestSoFar, currentLength);
+        }
+        return bestSoFar;
+    }
+
+
 }
