db4ee6344cbbf84a4b9a4b20fa8bd6c3a1fd1b6a
sandell <sandell@crashandlearn.se>
2018-12-06 07:02:16 +0100

    Day6a

--- src/main/java/day06/Day06.java
+++ src/main/java/day06/Day06.java
@@ -1,4 +1,106 @@
 package day06;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day06 {
+    static class Coordinate {
+        int x, y;
+        int area=0;
+        boolean isNextToWall =false;
+        Coordinate(int x, int y) {
+            this.x=x;
+            this.y=y;
+        }
+        int getX() {return x;}
+        int getY() {return y;}
+        void addToArea(int i) {
+            area+=i;
+        }
+        void setIsNexTToWall(boolean state) {
+            isNextToWall =state;
+        }
+        boolean isNextToWall() {return isNextToWall;}
+        int getArea() {return area;}
+
+        int getDistance(int x, int y) {
+            return Math.abs(getX()-x) + Math.abs(getY()-y);
+        }
+    }
+
+    static ArrayList<Coordinate> splitInput(String input) {
+        ArrayList<Coordinate> coordinates = new ArrayList<>();
+        String[] rows = input.split("\n");
+        for (String row : rows) {
+            int x = Integer.valueOf(row.split(", ")[0]);
+            int y = Integer.valueOf(row.split(", ")[1]);
+            coordinates.add(new Coordinate(x,y));
+        }
+        return coordinates;
+    }
+    static int findClosestCoordinate(int x, int y, List<Coordinate> coordinates) {
+        int closestDistance = 999;
+        int closestPoint = -1;
+
+        for (int i=0; i<coordinates.size(); i++) {
+            int distance = coordinates.get(i).getDistance(x,y);
+            if (distance<closestDistance) {
+                closestPoint=i;
+                closestDistance=distance;
+
+            } else if (distance == closestDistance){
+                closestPoint=-1;
+                closestDistance=distance;
+            }
+        }
+        if (closestPoint>=0) {
+            coordinates.get(closestPoint).addToArea(1);
+            if (x == 0 || x == 399 || y == 0 || y == 399) {
+                coordinates.get(closestPoint).setIsNexTToWall(true);
+            }
+        }
+        return closestPoint;
+
+    }
+    static int totalDistance(int x, int y, List<Coordinate> coordinates) {
+        int distance=0;
+        for (Coordinate c : coordinates) {
+            distance += c.getDistance(x,y);
+        }
+        return distance;
+    }
+
+    static int largestFiniteArea(String input) {
+        ArrayList<Coordinate> locations = splitInput(input);
+        int size=400;
+        int grid[][] = new int[size][size];
+        for (int x=0;x<size; x++) {
+            for (int y = 0; y < size; y++) {
+                grid[x][y] = findClosestCoordinate(x, y, locations);
+            }
+        }
+        int largestArea=0;
+        for (Coordinate c : locations) {
+            if (!c.isNextToWall) {
+                largestArea=Math.max(largestArea, c.getArea());
+            }
+        }
+        return largestArea;
+    }
+
+    static int regionWithTotalDistanceLessThan(String input, int distanceLimit) {
+        ArrayList<Coordinate> locations = splitInput(input);
+        int regionSize=0;
+        int size=400;
+
+        for (int x=0;x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (totalDistance(x, y, locations) < distanceLimit) {
+                    regionSize++;
+                }
+            }
+        }
+        return regionSize;
+    }
+
 }
--- src/test/java/day06/Day06Test.java
+++ src/test/java/day06/Day06Test.java
@@ -2,15 +2,87 @@ package day06;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static day06.Day06.largestFiniteArea;
+import static day06.Day06.regionWithTotalDistanceLessThan;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day06Test {
 
+    @Test
+    public void exampleOne() {
+        String input = "1, 1\n" +
+                        "1, 6\n" +
+                        "8, 3\n" +
+                        "3, 4\n" +
+                        "5, 5\n" +
+                        "8, 9";
 
+        assertEquals(17, largestFiniteArea(input));
+    }
+    @Test
+    public void exampleTwo() {
+        String input = "1, 1\n" +
+                "1, 6\n" +
+                "8, 3\n" +
+                "3, 4\n" +
+                "5, 5\n" +
+                "8, 9";
+
+        assertEquals(16, regionWithTotalDistanceLessThan(input,32));
+    }
     @Test
     void puzzle() {
-        String input="";
-        System.out.println("Day06(a): ");
-        System.out.println("Day06(b): ");
+        String input="152, 292\n" +
+                "163, 90\n" +
+                "258, 65\n" +
+                "123, 147\n" +
+                "342, 42\n" +
+                "325, 185\n" +
+                "69, 45\n" +
+                "249, 336\n" +
+                "92, 134\n" +
+                "230, 241\n" +
+                "74, 262\n" +
+                "241, 78\n" +
+                "299, 58\n" +
+                "231, 146\n" +
+                "239, 87\n" +
+                "44, 157\n" +
+                "156, 340\n" +
+                "227, 226\n" +
+                "212, 318\n" +
+                "194, 135\n" +
+                "235, 146\n" +
+                "171, 197\n" +
+                "160, 59\n" +
+                "218, 205\n" +
+                "323, 102\n" +
+                "290, 356\n" +
+                "244, 214\n" +
+                "174, 250\n" +
+                "70, 331\n" +
+                "288, 80\n" +
+                "268, 128\n" +
+                "359, 98\n" +
+                "78, 249\n" +
+                "221, 48\n" +
+                "321, 228\n" +
+                "52, 225\n" +
+                "151, 302\n" +
+                "183, 150\n" +
+                "142, 327\n" +
+                "172, 56\n" +
+                "72, 321\n" +
+                "225, 298\n" +
+                "265, 300\n" +
+                "86, 288\n" +
+                "78, 120\n" +
+                "146, 345\n" +
+                "268, 181\n" +
+                "243, 235\n" +
+                "262, 268\n" +
+                "40, 60";
+        System.out.println("Day06(a): "+largestFiniteArea(input));
+        System.out.println("Day06(b): "+regionWithTotalDistanceLessThan(input,10000)); //40134
     }
 }
