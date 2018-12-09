88c05b7d23b7a403cd722aaedaa424820aa1185f
sandell <sandell@crashandlearn.se>
2018-12-09 08:30:37 +0100

    Day9

--- src/main/java/day09/Day09.java
+++ src/main/java/day09/Day09.java
@@ -1,11 +1,94 @@
 package day09;
 
+import java.util.Arrays;
+
 class Day09 {
-    static String solveA(String input) {
-        return "";
+    static long solveA(String input) {
+        int players = Integer.valueOf(input.split(" ")[0]);
+        int maxMarble = Integer.valueOf(input.split(" ")[6]);
+        return allTheMarbles(players,maxMarble);
+    }
+    static long solveB(String input) {
+        int players = Integer.valueOf(input.split(" ")[0]);
+        int maxMarble = Integer.valueOf(input.split(" ")[6]);
+        return allTheMarbles(players,maxMarble*100);
+    }
+    private static long allTheMarbles(int players, int maxMarbleValue) {
+        Marble startMarble = new Marble();
+
+        Long[] scores = new Long[players];
+        for (int i =0; i< players; i++) {
+            scores[i]=0L;
+        }
+
+        int currentPlayer=0;
+        Marble currentMarble = startMarble;
+
+        for (int currentValue=1; currentValue <= maxMarbleValue; currentValue++) {
+            if (currentValue%23 > 0) {
+                currentMarble=currentMarble.getNextMarble();
+                currentMarble=addMarble(currentValue, currentMarble);
+            } else {
+                scores[currentPlayer] += currentValue;
+                currentMarble=currentMarble.getPreviousMarble()
+                                           .getPreviousMarble()
+                                           .getPreviousMarble()
+                                           .getPreviousMarble()
+                                           .getPreviousMarble()
+                                           .getPreviousMarble()
+                                           .getPreviousMarble();
+                scores[currentPlayer]+=currentMarble.getValue();
+                currentMarble=removeMarble(currentMarble);
+            }
+            currentPlayer=(currentPlayer+1)%players;
+        }
+        return Arrays.stream(scores).mapToLong(Long::valueOf).max().orElse(-1L);
     }
 
-    static String solveB(String input) {
-        return "";
+    private static Marble addMarble(int value, Marble currentMarble) {
+        Marble newMarble = new Marble(currentMarble, currentMarble.getNextMarble(), value);
+        newMarble.getPreviousMarble().setNextMarble(newMarble);
+        newMarble.getNextMarble().setPreviousMarble(newMarble);
+        return newMarble;
+    }
+    private static Marble removeMarble(Marble currentMarble) {
+        currentMarble.getPreviousMarble().setNextMarble(currentMarble.getNextMarble());
+        currentMarble.getNextMarble().setPreviousMarble(currentMarble.getPreviousMarble());
+        return currentMarble.getNextMarble();
+    }
+    private static class Marble {
+        private Marble previousMarble;
+        private Marble nextMarble;
+        private int value;
+        Marble () {
+            value=0;
+            nextMarble=this;
+            previousMarble=this;
+        }
+
+        Marble(Marble previousMarble, Marble nextMarble, int value) {
+            this.previousMarble=previousMarble;
+            this.nextMarble=nextMarble;
+            this.value=value;
+        }
+
+        Marble getPreviousMarble() {
+            return previousMarble;
+        }
+
+        void setPreviousMarble(Marble previousMarble) {
+            this.previousMarble=previousMarble;
+        }
+
+        Marble getNextMarble() {
+            return nextMarble;
+        }
+        void setNextMarble(Marble nextMarble) {
+            this.nextMarble=nextMarble;
+        }
+        int getValue() {
+            return value;
+        }
+
     }
 }
--- src/test/java/day09/Day09Test.java
+++ src/test/java/day09/Day09Test.java
@@ -3,21 +3,43 @@ package day09;
 import org.junit.jupiter.api.Test;
 import utils.Utils;
 
-import static day09.Day09.solveA;
-import static day09.Day09.solveB;
+import static day09.Day09.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day09Test {
     @Test
-    void example_A() {
-        String input = "";
-        assertEquals("", solveA(input));
+    void example_A0() {
+        String input = "9 players; last marble is worth 25 points";
+        assertEquals(32, solveA(input));
+
     }
     @Test
-    void example_B() {
-        String input = "";
-        assertEquals("", solveB(input));
+    void example_A1() {
+        String input = "10 players; last marble is worth 1618 points";
+        assertEquals(8317, solveA(input));
     }
+
+    @Test
+    void example_A2() {
+        String input = "13 players; last marble is worth 7999 points:";
+        assertEquals(146373, solveA(input));
+    }
+    @Test
+    void example_A3() {
+        String input = "17 players; last marble is worth 1104 points";
+        assertEquals(2764, solveA(input));
+    }
+    @Test
+    void example_A4() {
+        String input ="21 players; last marble is worth 6111 points";
+        assertEquals(54718, solveA(input));
+    }
+    @Test
+    void example_A5() {
+        String input = "30 players; last marble is worth 5807 points";
+        assertEquals(37305, solveA(input));
+    }
+
     @Test
     void puzzle() {
         String day= "09";
