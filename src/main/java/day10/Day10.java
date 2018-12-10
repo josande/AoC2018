ff7bef364227a843ff7f1e8c86bd453f963a7e59
sandell <sandell@crashandlearn.se>
2018-12-10 08:06:44 +0100

    Day10

--- src/main/java/day10/Day10.java
+++ src/main/java/day10/Day10.java
@@ -1,11 +1,115 @@
 package day10;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Day10 {
     static String solveA(String input) {
+        String[] rows = splitInput(input);
+
+        Pair<List<Pair<Integer, Integer>>,List<Pair<Integer, Integer>>> starPositionMovement = getStarsPositionAndMovement(rows);
+
+        int relevantTime= findMin(starPositionMovement.getKey(), starPositionMovement.getValue());
+        print(starPositionMovement.getKey(), starPositionMovement.getValue(), relevantTime);
         return "";
     }
 
-    static String solveB(String input) {
-        return "";
+    private static int findMin(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements) {
+        int sizeX=-1;
+
+        for (int time=0; ; time++) {
+                int maxX=getMaxX(coordinates, movements, time);
+                int minX=getMinX(coordinates, movements, time);
+
+                if (( sizeX<0 || Math.abs(maxX-minX) < sizeX )){
+                    sizeX=Math.abs(maxX-minX);
+                } else {
+                    return time-1;
+                }
+        }
+    }
+
+    private static void print(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements, int time) {
+        int minX=getMinX(coordinates, movements, time);
+        int minY=getMinY(coordinates, movements, time);
+        int maxX=getMaxX(coordinates, movements, time);
+        int maxY=getMaxY(coordinates, movements, time);
+        Character[][] plot = new Character[maxX-minX+2][maxY-minY+2];
+
+        for (int x = 0; x<=maxX-minX+1; x++) {
+            for (int y = 0; y <= maxY-minY+1; y++) {
+                plot[x][y] = ' ';
+            }
+        }
+        for (int star = 0; star < coordinates.size(); star++) {
+            int x = coordinates.get(star).getKey() + movements.get(star).getKey() * time;
+            int y = coordinates.get(star).getValue() + movements.get(star).getValue() * time;
+            plot[x-minX][y-minY] = '#';
+        }
+            for (int y = 0; y <= maxY-minY; y++) {
+                for (int x = 0; x<=maxX-minX; x++) {
+                    System.out.print(plot[x][y]);
+                }
+
+            System.out.print("\n");
+        }
+    }
+
+    private static int getMinX(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements, int time) {
+        int minX = 9999999;
+        for (int star = 0; star < coordinates.size(); star++) {
+            minX = Math.min(minX, coordinates.get(star).getKey() + movements.get(star).getKey() * time);
+        }
+        return minX;
+    }
+    private static int getMinY(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements, int time) {
+        int minY = 9999999;
+        for (int star = 0; star < coordinates.size(); star++) {
+            minY = Math.min(minY, coordinates.get(star).getValue() + movements.get(star).getValue() * time);
+        }
+        return minY;
+    }
+    private static int getMaxX(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements, int time) {
+        int maxX = 0;
+        for (int star = 0; star < coordinates.size(); star++) {
+            maxX = Math.max(maxX, coordinates.get(star).getKey() + movements.get(star).getKey() * time);
+        }
+        return maxX;
+    }
+    private static int getMaxY(List<Pair<Integer, Integer>> coordinates, List<Pair<Integer, Integer>>  movements, int time) {
+        int maxY = 0;
+        for (int star = 0; star < coordinates.size(); star++) {
+            maxY = Math.max(maxY, coordinates.get(star).getValue() + movements.get(star).getValue() * time);
+        }
+        return maxY;
+    }
+
+     static int solveB(String input) {
+         String[] rows = splitInput(input);
+         Pair<List<Pair<Integer, Integer>>,List<Pair<Integer, Integer>>> starPositionMovement = getStarsPositionAndMovement(rows);
+         return findMin(starPositionMovement.getKey(), starPositionMovement.getValue());
+    }
+    private static Pair<List<Pair<Integer, Integer>>,List<Pair<Integer, Integer>>> getStarsPositionAndMovement(String[] dataRows) {
+        List<Pair<Integer, Integer>> coordinates = new ArrayList<>();
+        List<Pair<Integer, Integer>> movements = new ArrayList<>();
+
+        for (String row: dataRows) {
+            String[] values = row.split(" ");
+            coordinates.add(new Pair<>(Integer.valueOf(values[0]), Integer.valueOf(values[1])));
+            movements.add(new Pair<>(Integer.valueOf(values[2]), Integer.valueOf(values[3])));
+        }
+        return new Pair<>(coordinates, movements);
+    }
+    private static String[] splitInput(String input) {
+        return input
+                .replaceAll("position=< ","")
+                .replaceAll("position=<","")
+                .replaceAll("velocity=<","")
+                .replaceAll(">","")
+                .replaceAll(",","")
+                .replaceAll(" {2,}"," ")
+                .split("\r?\n");
     }
 }
--- src/test/java/day10/Day10Test.java
+++ src/test/java/day10/Day10Test.java
@@ -11,19 +11,79 @@ class Day10Test {
 
     @Test
     void example_A() {
-        String input = "";
+        String input = "position=< 9,  1> velocity=< 0,  2>\n" +
+                "position=< 7,  0> velocity=<-1,  0>\n" +
+                "position=< 3, -2> velocity=<-1,  1>\n" +
+                "position=< 6, 10> velocity=<-2, -1>\n" +
+                "position=< 2, -4> velocity=< 2,  2>\n" +
+                "position=<-6, 10> velocity=< 2, -2>\n" +
+                "position=< 1,  8> velocity=< 1, -1>\n" +
+                "position=< 1,  7> velocity=< 1,  0>\n" +
+                "position=<-3, 11> velocity=< 1, -2>\n" +
+                "position=< 7,  6> velocity=<-1, -1>\n" +
+                "position=<-2,  3> velocity=< 1,  0>\n" +
+                "position=<-4,  3> velocity=< 2,  0>\n" +
+                "position=<10, -3> velocity=<-1,  1>\n" +
+                "position=< 5, 11> velocity=< 1, -2>\n" +
+                "position=< 4,  7> velocity=< 0, -1>\n" +
+                "position=< 8, -2> velocity=< 0,  1>\n" +
+                "position=<15,  0> velocity=<-2,  0>\n" +
+                "position=< 1,  6> velocity=< 1,  0>\n" +
+                "position=< 8,  9> velocity=< 0, -1>\n" +
+                "position=< 3,  3> velocity=<-1,  1>\n" +
+                "position=< 0,  5> velocity=< 0, -1>\n" +
+                "position=<-2,  2> velocity=< 2,  0>\n" +
+                "position=< 5, -2> velocity=< 1,  2>\n" +
+                "position=< 1,  4> velocity=< 2,  1>\n" +
+                "position=<-2,  7> velocity=< 2, -2>\n" +
+                "position=< 3,  6> velocity=<-1, -1>\n" +
+                "position=< 5,  0> velocity=< 1,  0>\n" +
+                "position=<-6,  0> velocity=< 2,  0>\n" +
+                "position=< 5,  9> velocity=< 1, -2>\n" +
+                "position=<14,  7> velocity=<-2,  0>\n" +
+                "position=<-3,  6> velocity=< 2, -1>";
         assertEquals("", solveA(input));
     }
     @Test
     void example_B() {
-        String input = "";
-        assertEquals("", solveB(input));
+        String input = "position=< 9,  1> velocity=< 0,  2>\n" +
+                "position=< 7,  0> velocity=<-1,  0>\n" +
+                "position=< 3, -2> velocity=<-1,  1>\n" +
+                "position=< 6, 10> velocity=<-2, -1>\n" +
+                "position=< 2, -4> velocity=< 2,  2>\n" +
+                "position=<-6, 10> velocity=< 2, -2>\n" +
+                "position=< 1,  8> velocity=< 1, -1>\n" +
+                "position=< 1,  7> velocity=< 1,  0>\n" +
+                "position=<-3, 11> velocity=< 1, -2>\n" +
+                "position=< 7,  6> velocity=<-1, -1>\n" +
+                "position=<-2,  3> velocity=< 1,  0>\n" +
+                "position=<-4,  3> velocity=< 2,  0>\n" +
+                "position=<10, -3> velocity=<-1,  1>\n" +
+                "position=< 5, 11> velocity=< 1, -2>\n" +
+                "position=< 4,  7> velocity=< 0, -1>\n" +
+                "position=< 8, -2> velocity=< 0,  1>\n" +
+                "position=<15,  0> velocity=<-2,  0>\n" +
+                "position=< 1,  6> velocity=< 1,  0>\n" +
+                "position=< 8,  9> velocity=< 0, -1>\n" +
+                "position=< 3,  3> velocity=<-1,  1>\n" +
+                "position=< 0,  5> velocity=< 0, -1>\n" +
+                "position=<-2,  2> velocity=< 2,  0>\n" +
+                "position=< 5, -2> velocity=< 1,  2>\n" +
+                "position=< 1,  4> velocity=< 2,  1>\n" +
+                "position=<-2,  7> velocity=< 2, -2>\n" +
+                "position=< 3,  6> velocity=<-1, -1>\n" +
+                "position=< 5,  0> velocity=< 1,  0>\n" +
+                "position=<-6,  0> velocity=< 2,  0>\n" +
+                "position=< 5,  9> velocity=< 1, -2>\n" +
+                "position=<14,  7> velocity=<-2,  0>\n" +
+                "position=<-3,  6> velocity=< 2, -1>";
+        assertEquals(3, solveB(input));
     }
     @Test
     void puzzle() {
         String day= "10";
         String input=new Utils().readFile("Day"+day+"Input.txt");
-        System.out.println("Day"+day+"(a): " + solveA(input));
+        System.out.println("Day"+day+"(a): "); solveA(input);
         System.out.println("Day"+day+"(b): " + solveB(input));
     }
 
