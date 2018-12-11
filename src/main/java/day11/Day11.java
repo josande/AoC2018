dcf94993d97fa4a2807de5b32c59bc4cac8266c0
sandell <sandell@crashandlearn.se>
2018-12-11 07:11:00 +0100

    Day11

--- src/main/java/day11/Day11.java
+++ src/main/java/day11/Day11.java
@@ -1,11 +1,65 @@
 package day11;
 
+import javafx.util.Pair;
+
 class Day11 {
     static String solveA(String input) {
-        return "";
+        int[][] powerGrid = new int[300][300];
+        for (int x=0; x<300; x++) {
+            for (int y=0; y< 300; y++) {
+                powerGrid[x][y]=(int) getPowerLevel(x+1, y+1, Integer.valueOf(input));
+            }
+        }
+        return getMaxPowerSquare(powerGrid, 3).getKey();
+    }
+    private static Pair<String, Integer> getMaxPowerSquare(int[][] powerGrid, int size) {
+        int maxPower=0;
+        int maxX=-1;
+        int maxY=-1;
+        for (int x=0; x < 300-size; x++) {
+            for (int y = 0; y < 300-size; y++) {
+                int sum = 0;
+                for (int sx = x; sx<x+size; sx++ ){
+                    for (int sy = y; sy<y+size; sy++ ) {
+                        sum+=powerGrid[sx][sy];
+                    }
+                }
+                if (sum>maxPower) {
+                    maxPower=sum;
+                    maxX=x+1;
+                    maxY=y+1;
+                }
+            }
+        }
+        return new Pair<>(maxX+","+maxY, maxPower);
+
+    }
+
+    static long getPowerLevel(int x, int y, int serialNumber) { //3,5 8
+        long rackId = x+10L;
+        long power = rackId*y+serialNumber;
+        power*=rackId;
+        return (power%1000/100-5);
     }
 
     static String solveB(String input) {
-        return "";
+        int[][] powerGrid = new int[300][300];
+        for (int x=0; x<300; x++) {
+            for (int y=0; y< 300; y++) {
+                powerGrid[x][y]=(int) getPowerLevel(x+1, y+1, Integer.valueOf(input));
+            }
+        }
+        int maxPower=0;
+        int maxSize=0;
+        String coordinate="";
+        for (int size=1; size <=300; size++) {
+            Pair<String, Integer> result = getMaxPowerSquare(powerGrid, size);
+            if  (result.getValue() > maxPower) {
+                maxPower=result.getValue();
+                coordinate=result.getKey();
+                maxSize=size;
+            }
+        }
+        return coordinate+","+maxSize;
     }
 }
--- src/test/java/day11/Day11Test.java
+++ src/test/java/day11/Day11Test.java
@@ -3,6 +3,7 @@ package day11;
 import org.junit.jupiter.api.Test;
 import utils.Utils;
 
+import static day11.Day11.getPowerLevel;
 import static day11.Day11.solveA;
 import static day11.Day11.solveB;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,13 +13,20 @@ class Day11Test {
 
     @Test
     void example_A() {
-        String input = "";
-        assertEquals("", solveA(input));
+        assertEquals(4, getPowerLevel(3,5,8));
+        assertEquals(-5,getPowerLevel(122,79,57));
+        assertEquals(0, getPowerLevel(217,196,39));
+        assertEquals(4, getPowerLevel(101,153,71));
+
+        assertEquals("21,61",solveA("42"));
+        assertEquals("33,45",solveA("18"));
+
     }
     @Test
     void example_B() {
-        String input = "";
-        assertEquals("", solveB(input));
+        assertEquals("90,269,16", solveB("18"));
+        assertEquals("232,251,12", solveB("42"));
+
     }
     @Test
     void puzzle() {
