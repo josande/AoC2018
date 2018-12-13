f6447f76cd2d47e8923c4ef0555491e6ef706689
sandell <sandell@crashandlearn.se>
2018-12-13 22:55:46 +0100

    Day13

--- src/main/java/day13/Day13.java
+++ src/main/java/day13/Day13.java
@@ -27,11 +27,9 @@ class Day13 {
                 dy = 0;
             }
             if (c == 'v') {
-                dy = 1;
                 dx = 0;
+                dy = 1;
             }
-            System.out.println("New cart "+y+","+x+", "+c+" dx:"+dx+" dy:"+dy);
-
         }
 
         int getX() {
@@ -48,53 +46,87 @@ class Day13 {
                 return -1;
             else if (this.getY() > other.getY())
                 return 1;
-            return other.getX()-this.getX();
+            return this.getX()-other.getX();
         }
 
-        boolean move(HashMap<Pair<Integer, Integer>, Character> map, List<Cart> carts, int iteration) {
+        ArrayList<Cart> move(HashMap<Pair<Integer, Integer>, Character> map, List<Cart> carts, int iteration) {
             Pair<Integer, Integer> location = new Pair<>(y+dy,x+dx);
             x=x+dx;
             y=y+dy;
-            if (!isFree(location, carts)) {
-                return false;
-            }
+            ArrayList<Cart> cartsHere=cartsAtEndPosition(location, carts);
 
-            char nextPlace= map.getOrDefault(location, ' ');
+            Character stopsAt= map.get(location);
+            if (stopsAt == null)
+                return cartsHere;
 
-            int tmp=dx;
-            if (nextPlace == '\\') {
+            int tmpdX=dx;
+            if (stopsAt == '\\') {
                  dx=dy;
-                 dy=tmp;
+                 dy=tmpdX;
 
-            } else if (nextPlace == '/') {
+            } else if (stopsAt == '/') {
                 dx=-dy;
-                dy=-tmp;
+                dy=-tmpdX;
 
-            } else if (nextPlace == '+') {
+            } else if (stopsAt == '+') {
                 if (turns%3 == 0) {
+
+                    //   >
+                    // v   ^
+                    //   >
+                    //
+
+                    //   v     dx dy  dx dy
+                    //1    >   (0,1)  (1,0)          dy=-dx   dx=dy
+                    //
+
+                    //2    ^  dx dy  dx dy
+                    //   >    (1,0)  (0,-1)          dy=-dx   dx=dy
+
+                    //3  <    dx dy  dx dy
+                    //     ^  (0,-1) (-1,0)          dy=-dx   dx=dy
+
+                    //4    <  dx dy  dx dy
+                    //   v    (-1,0) (0,1)           dy=-dx   dx=dy
+
+                    if (dx == 0) {
+
+                    }
                     dx=dy;
-                    dy=tmp;
+                    dy=-tmpdX;
                 } else if (turns%3 == 2) {
+
+                    //1    v  dx dy  dx dy
+                    //   <    (0,1) (-1,0)          dy=dx   dx=-dy
+                    //
+
+                    //2  >    dx dy  dx dy
+                    //    v   (1,0)  (0,1)           dy=dx    dx=-dy
+
+                    //3    >  dx dy  dx dy
+                    //   ^    (0,-1) (1,0)           dy=dx   dx=-dy
+
+                    //4  ^    dx dy  dx dy
+                    //     < (-1,0)  (0,-1)          dy=dx   dx=-dy
+
                     dx=-dy;
-                    dy=-tmp;
+                    dy=tmpdX;
                 }
                 turns++;
 
             }
 
-            return true;
+            return cartsHere;
         }
 
-        private boolean isFree(Pair<Integer, Integer> location, List<Cart> carts) {
-            int cartsHere=0;
+        private ArrayList<Cart> cartsAtEndPosition(Pair<Integer, Integer> location, List<Cart> carts) {
+            ArrayList<Cart> cartsHere=new ArrayList<>();
             for (Cart cart: carts) {
                 if (cart.getX()==location.getValue() && cart.getY()==location.getKey()) {
-                    cartsHere++;
-                    if (cartsHere>1)
-                        return false;
+                    cartsHere.add(cart);
                 }
             }
-            return true;
+            return cartsHere;
         }
 
     }
@@ -109,32 +141,56 @@ class Day13 {
                 char curr= rows[row].charAt(col);
                 if (curr=='^' ||curr=='v' || curr=='<' ||curr=='>') {
                     carts.add(new Cart(row, col, curr));
-
-                }
-                if (curr=='/' || curr=='\\'  || curr=='+' ) {
+                } else if (curr=='/' || curr=='\\'  || curr=='+' ) {
                     map.put(coordinate, curr);
                 }
             }
         }
 
         //Move da cars!
-
-
-        System.out.println("MapSize: "+map.size());
         for (int iteration = 0; ; iteration++) {
             Collections.sort(carts);
             for (Cart cart : carts) {
-            //    System.out.println(cart.getY()+","+cart.getX());
-                if (!cart.move(map, carts,iteration)) {
-                    System.out.println(" KABLOOM @ "+cart.getX()+", "+cart.getY());
+                if (cart.move(map, carts,iteration).size()>1) {
                     return cart.getX()+","+cart.getY();
                 }
             }
-
         }
     }
 
     static String solveB(String input) {
-        return "";
+        String[] rows = input.split("\r?\n");
+        ArrayList<Cart> carts = new ArrayList<>();
+        HashMap<Pair<Integer, Integer>, Character> map = new HashMap<>();
+        for (int row=0; row<rows.length; row++) {
+            for (int col=0; col< rows[row].length(); col++) {
+                Pair<Integer, Integer> coordinate = new Pair<>(row, col);
+                char curr= rows[row].charAt(col);
+                if (curr=='^' ||curr=='v' || curr=='<' ||curr=='>') {
+                    carts.add(new Cart(row, col, curr));
+                } else if (curr=='/' || curr=='\\'  || curr=='+' ) {
+                    map.put(coordinate, curr);
+                }
+            }
+        }
+
+        //Move da cars!
+        for (int iteration = 0; ; iteration++) {
+            Collections.sort(carts);
+            Iterator<Cart> cart = carts.iterator();
+            ArrayList<Cart> cartsToRemove=new ArrayList<>();
+            while(cart.hasNext()) {
+                Cart currentCart = cart.next();
+                ArrayList<Cart> cartsHere=currentCart.move(map, carts,iteration);
+                if (cartsHere.size()==2) {
+                    cartsToRemove.addAll(cartsHere);
+                }
+            }
+            carts.removeAll(cartsToRemove);
+
+            if (carts.size() == 1) {
+                return carts.get(0).getX() + "," + carts.get(0).getY();
+            }
+        }
     }
 }
--- src/test/java/day13/Day13Test.java
+++ src/test/java/day13/Day13Test.java
@@ -3,8 +3,10 @@ package day13;
 import org.junit.jupiter.api.Test;
 import utils.Utils;
 
-import static day13.Day13.solveA;
-import static day13.Day13.solveB;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static day13.Day13.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day13Test {
@@ -20,10 +22,38 @@ class Day13Test {
                        "  \\------/   ";
         assertEquals("7,3", solveA(input));
     }
+
+    @Test
+    public void testSorting() {
+        ArrayList<Cart> carts = new ArrayList<>();
+        Cart a = new Cart(2,2, '<');
+        Cart b = new Cart(1,1, '<');
+        Cart c = new Cart(1,3, '<');
+        Cart d = new Cart(0,2, '<');
+        carts.add(a);
+        carts.add(b);
+        carts.add(c);
+        carts.add(d);
+        assertEquals(a, carts.get(0));
+        assertEquals(b, carts.get(1));
+        assertEquals(c, carts.get(2));
+        assertEquals(d, carts.get(3));
+        Collections.sort(carts);
+        assertEquals(d, carts.get(0));
+        assertEquals(b, carts.get(1));
+        assertEquals(c, carts.get(2));
+        assertEquals(a, carts.get(3));
+    }
     @Test
     void example_B() {
-        String input = "";
-        assertEquals("", solveB(input));
+        String input = "/>-<\\  \n" +
+                       "|   |  \n" +
+                       "| /<+-\\\n" +
+                       "| | | v\n" +
+                       "\\>+</ |\n" +
+                       "  |   ^\n" +
+                       "  \\<->/\n";
+        assertEquals("6,4", solveB(input));
     }
     @Test
     void puzzle() {
