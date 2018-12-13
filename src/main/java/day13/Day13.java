d46ed55c8d11ef4420f27002001d6c0493032498
sandell <sandell@crashandlearn.se>
2018-12-13 23:15:55 +0100

    Day13

--- src/main/java/day13/Day13.java
+++ src/main/java/day13/Day13.java
@@ -13,113 +13,54 @@ class Day13 {
         Cart(int y, int x, char c) {
             this.x=x;
             this.y=y;
-
-            if (c == '<'){
-                dx=-1;
-                dy=0;
-            }
-            if (c == '^') {
-                dx = 0;
-                dy = -1;
-            }
-            if (c == '>') {
-                dx = 1;
-                dy = 0;
-            }
-            if (c == 'v') {
-                dx = 0;
-                dy = 1;
-            }
+            if (c == '<') { dx = -1; dy =  0; }
+            if (c == '^') { dx =  0; dy = -1; }
+            if (c == '>') { dx =  1; dy =  0; }
+            if (c == 'v') { dx =  0; dy =  1; }
         }
 
-        int getX() {
-            return x;
-        }
-        int getY() {
-            return y;
-        }
-
+        int getX() { return x; }
+        int getY() { return y; }
 
         @Override
         public int compareTo(Cart other){
-            if (this.getY() < other.getY())
-                return -1;
-            else if (this.getY() > other.getY())
-                return 1;
-            return this.getX()-other.getX();
+            if (this.getY() == other.getY()) return this.getX()-other.getX();
+            return  this.getY() - other.getY();
         }
 
-        ArrayList<Cart> move(HashMap<Pair<Integer, Integer>, Character> map, List<Cart> carts, int iteration) {
+        ArrayList<Cart> move(HashMap<Pair<Integer, Integer>, Character> map, List<Cart> carts) {
             Pair<Integer, Integer> location = new Pair<>(y+dy,x+dx);
             x=x+dx;
             y=y+dy;
-            ArrayList<Cart> cartsHere=cartsAtEndPosition(location, carts);
+
+            ArrayList<Cart> cartsHere= cartsAtLocation(location, carts);
 
             Character stopsAt= map.get(location);
-            if (stopsAt == null)
-                return cartsHere;
+            if (stopsAt != null) {
+                int tmpX = dx;
+                if (stopsAt == '\\') {
+                    dx = dy;
+                    dy = tmpX;
 
-            int tmpdX=dx;
-            if (stopsAt == '\\') {
-                 dx=dy;
-                 dy=tmpdX;
+                } else if (stopsAt == '/') {
+                    dx = -dy;
+                    dy = -tmpX;
 
-            } else if (stopsAt == '/') {
-                dx=-dy;
-                dy=-tmpdX;
-
-            } else if (stopsAt == '+') {
-                if (turns%3 == 0) {
-
-                    //   >
-                    // v   ^
-                    //   >
-                    //
-
-                    //   v     dx dy  dx dy
-                    //1    >   (0,1)  (1,0)          dy=-dx   dx=dy
-                    //
-
-                    //2    ^  dx dy  dx dy
-                    //   >    (1,0)  (0,-1)          dy=-dx   dx=dy
-
-                    //3  <    dx dy  dx dy
-                    //     ^  (0,-1) (-1,0)          dy=-dx   dx=dy
-
-                    //4    <  dx dy  dx dy
-                    //   v    (-1,0) (0,1)           dy=-dx   dx=dy
-
-                    if (dx == 0) {
-
+                } else if (stopsAt == '+') {
+                    if (turns % 3 == 0) {
+                        dx = dy;
+                        dy = -tmpX;
+                    } else if (turns % 3 == 2) {
+                        dx = -dy;
+                        dy = tmpX;
                     }
-                    dx=dy;
-                    dy=-tmpdX;
-                } else if (turns%3 == 2) {
-
-                    //1    v  dx dy  dx dy
-                    //   <    (0,1) (-1,0)          dy=dx   dx=-dy
-                    //
-
-                    //2  >    dx dy  dx dy
-                    //    v   (1,0)  (0,1)           dy=dx    dx=-dy
-
-                    //3    >  dx dy  dx dy
-                    //   ^    (0,-1) (1,0)           dy=dx   dx=-dy
-
-                    //4  ^    dx dy  dx dy
-                    //     < (-1,0)  (0,-1)          dy=dx   dx=-dy
-
-                    dx=-dy;
-                    dy=tmpdX;
+                    turns++;
                 }
-                turns++;
-
             }
-
             return cartsHere;
         }
 
-        private ArrayList<Cart> cartsAtEndPosition(Pair<Integer, Integer> location, List<Cart> carts) {
+        private ArrayList<Cart> cartsAtLocation(Pair<Integer, Integer> location, List<Cart> carts) {
             ArrayList<Cart> cartsHere=new ArrayList<>();
             for (Cart cart: carts) {
                 if (cart.getX()==location.getValue() && cart.getY()==location.getKey()) {
@@ -128,7 +69,6 @@ class Day13 {
             }
             return cartsHere;
         }
-
     }
 
     static String solveA(String input) {
@@ -137,24 +77,21 @@ class Day13 {
         HashMap<Pair<Integer, Integer>, Character> map = new HashMap<>();
         for (int row=0; row<rows.length; row++) {
             for (int col=0; col< rows[row].length(); col++) {
-                Pair<Integer, Integer> coordinate = new Pair<>(row, col);
-                char curr= rows[row].charAt(col);
-                if (curr=='^' ||curr=='v' || curr=='<' ||curr=='>') {
-                    carts.add(new Cart(row, col, curr));
-                } else if (curr=='/' || curr=='\\'  || curr=='+' ) {
-                    map.put(coordinate, curr);
+                char currentChar= rows[row].charAt(col);
+                if ( currentChar=='^' || currentChar=='v' || currentChar=='<' || currentChar=='>') {
+                    carts.add(new Cart(row, col, currentChar));
+                } else if (currentChar=='/' || currentChar=='\\' || currentChar=='+' ) {
+                    map.put(new Pair<>(row, col), currentChar);
                 }
             }
         }
 
         //Move da cars!
-        for (int iteration = 0; ; iteration++) {
+        for (;;) {
             Collections.sort(carts);
-            for (Cart cart : carts) {
-                if (cart.move(map, carts,iteration).size()>1) {
+            for (Cart cart : carts)
+                if (cart.move(map, carts).size()>1)
                     return cart.getX()+","+cart.getY();
-                }
-            }
         }
     }
 
@@ -175,22 +112,19 @@ class Day13 {
         }
 
         //Move da cars!
-        for (int iteration = 0; ; iteration++) {
+        for (;;) {
             Collections.sort(carts);
-            Iterator<Cart> cart = carts.iterator();
             ArrayList<Cart> cartsToRemove=new ArrayList<>();
-            while(cart.hasNext()) {
-                Cart currentCart = cart.next();
-                ArrayList<Cart> cartsHere=currentCart.move(map, carts,iteration);
-                if (cartsHere.size()==2) {
+            Collections.sort(carts);
+            for (Cart cart : carts) {
+                ArrayList<Cart> cartsHere = cart.move(map, carts);
+                if (cartsHere.size()==2)
                     cartsToRemove.addAll(cartsHere);
-                }
             }
             carts.removeAll(cartsToRemove);
 
-            if (carts.size() == 1) {
+            if (carts.size() == 1)
                 return carts.get(0).getX() + "," + carts.get(0).getY();
-            }
         }
     }
 }
