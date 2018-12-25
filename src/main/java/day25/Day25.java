5eb32f35f7f802ee33f9293a93b5388a8a3937cd
sandell <sandell@crashandlearn.se>
2018-12-25 10:55:33 +0100

    day25

--- src/main/java/day25/Day25.java
+++ src/main/java/day25/Day25.java
@@ -1,78 +1,42 @@
 package day25;
-
-import javafx.util.Pair;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Day25 {
     static class Star {
-        int x,y,z,t;
-        HashSet<Integer> constellations = new HashSet<>();
-        Star(int x, int y, int z, int t) {
-            this.x=x;
-            this.y=y;
-            this.z=z;
-            this.t=t;
+        int[] values;
+        int constellation;
+        Star(int[] values, int constellation) {
+            this.values=values;
+            this.constellation=constellation;
         }
-        void setConstellation(int constellationNumber) {
-            constellations.add(constellationNumber);
+        void setConstellation(int constellation) {
+            this.constellation = constellation;
         }
-        void changeConstellation(int oldNumber, int newNumber) {
-            if (constellations.contains(oldNumber)) {
-                constellations.remove(oldNumber);
-                constellations.add(newNumber);
-            }
-        }
-        void addConstellations(HashSet<Integer> constellations) {
-            this.constellations.addAll(constellations);
-        }
-
-        HashSet<Integer> getConstellations() {
-            return constellations;
+        Integer getConstellation() {
+            return constellation;
         }
         boolean isWithinDistance(Star otherStar) {
-            return(Math.abs(x-otherStar.x)
-                  +Math.abs(y-otherStar.y)
-                  +Math.abs(z-otherStar.z)
-                  +Math.abs(t-otherStar.t)) <=3;
+            int d=0;
+            for ( int i=0; i<4; i++) { d+=Math.abs(values[i]-otherStar.values[i]); }
+            return d<=3;
         }
     }
 
-    static List<Star> knownStars;
-    static HashSet<Integer> allConstellations;
     static int solveA(String input) {
-        knownStars = new ArrayList<>();
-        allConstellations=new HashSet<>();
+        List<Star> knownStars = new ArrayList<>();
         int numberOfConstellations=0;
-
         for (String s : input.split("\r?\n")) {
-            String[] values = s.replaceAll(" ","").split(",");
-
-            int x=Integer.valueOf(values[0]);
-            int y=Integer.valueOf(values[1]);
-            int z=Integer.valueOf(values[2]);
-            int t=Integer.valueOf(values[3]);
-
-            Star star = new Star(x,y,z,t);
-
-            star.setConstellation(numberOfConstellations++);
-            allConstellations.add(numberOfConstellations);
-
-            knownStars.stream().filter(s1->s1.isWithinDistance(star))
-                               .forEach(s1->{star.addConstellations(s1.getConstellations());});
+            String[] values = s.replaceAll(" ", "").split(",");
+            Star star = new Star(Stream.of(values).mapToInt(Integer::parseInt).toArray(), numberOfConstellations++);
+            knownStars.stream().filter( s1 -> s1.isWithinDistance(star) )
+                               .forEach(s1 -> { star.setConstellation(s1.getConstellation()); });
             knownStars.add(star);
-            while (star.getConstellations().size()>1) {
-                List<Integer> constellations = new ArrayList<>(star.getConstellations());
-                allConstellations.remove(constellations.get(1));
-                knownStars.stream().filter(s1->s1.getConstellations()
-                                                 .contains(constellations.get(1)))
-                                                 .forEach(s1 ->{ s1.getConstellations().remove(constellations.get(1));
-                                                                 s1.getConstellations().add(constellations.get(0)); });
-            }
         }
-        return allConstellations.size();
+        return (int) knownStars.stream().mapToInt(Star::getConstellation).distinct().count();
     }
 }
